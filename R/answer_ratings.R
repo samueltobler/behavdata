cf5b904c34250262c7fc2d8d5802b945b1880995
Samuel Tobler <toblersa@ethz.ch>
2022-05-24 19:42:42 +0200

    Initial Commit

---
+++ R/answer_ratings.R
@@ -0,0 +1,42 @@
+#' Rating qualitative student answers
+#'
+#' @param
+#' @keywords answer; rating; qualitative
+#' @export
+#' @examples
+#'
+
+
+answer_ratings <- function(data, name = "", export = TRUE) {
+  
+  data_runner <- data[]
+  data_eval <- vector()
+  
+  for (i in 1:length(data_runner)) {
+    
+    print(paste("Participant ", i))
+    print(data_runner[i])
+    data_eval[i] <- readline(prompt="Rating: ")
+    
+  }
+  
+  
+  
+  if (export == TRUE) {
+    
+    evaluations <- as.integer(data_eval)
+    print(evaluations)
+    
+    if(dir.exists(file.path("CSV")) == TRUE) {} else {dir.create("CSV")}
+    
+    nametable <- paste("CSV/Ratings_", name,".csv", sep = "")
+    write.table(evaluations, nametable, sep = ";", col.names = FALSE, row.names = FALSE)
+    print("Your answers have been saved in a CSV file.") 
+    
+  } else {
+    evaluations <- as.integer(data_eval)
+    print(evaluations) 
+  }
+  
+  
+}
