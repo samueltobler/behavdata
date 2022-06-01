7dca6818eaa48a9af9cdcdee6a200d9ccf8dce9c
Samuel Tobler <toblersa@ethz.ch>
2022-06-01 22:27:18 +0200

    Correlation Matrix Function

--- R/correlation_table.R
+++ R/correlation_table.R
@@ -12,6 +12,16 @@ correlation_table <- function(data, name = "") {
   require(Hmisc)
   require(knitr)
   
+  flattenCorrMatrix <- function(cormat, pmat) {
+    ut <- upper.tri(cormat)
+    data.frame(
+      row = rownames(cormat)[row(cormat)[ut]],
+      column = rownames(cormat)[col(cormat)[ut]],
+      cor  =(cormat)[ut],
+      p = pmat[ut]
+    )
+  } # Function by sthda.com
+  
   resh <- rcorr(as.matrix(data))
   cortable.h <- flattenCorrMatrix(round(resh$r,2), round(resh$P,4))
   knitr::kable(cortable.h)
