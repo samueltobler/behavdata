5852505bd387ea290741bd18d1ba5263362c42ad
Samuel Tobler <toblersa@ethz.ch>
2022-06-24 11:42:28 +0200

    Print Output

--- R/eta_to_d.R
+++ R/eta_to_d.R
@@ -1,5 +1,6 @@
 eta_to_d <- function(eta) {
   att.f <- sqrt(eta[1] / (1-eta[1]))
   att.d <- sqrt(att.f^2 * 4)
-  output <- list("CohensD" = att.d)
+  output <- list("d" = att.d)
+  print(paste("Cohen's d = ",att.d, sep = ""))
 }
