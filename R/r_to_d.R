32437ae1205539b643efd4e1cfb67011367bdb91
Samuel <kontakt@samueltobler.ch>
2022-12-20 08:46:26 +0100

    Output Adjustment

--- R/r_to_d.R
+++ R/r_to_d.R
@@ -1,7 +1,6 @@
 r_to_d <- function(r, round = 2) {
-  d <- 2*r/sqrt((1-r^2))
+  d <- 2 * r/sqrt((1 - r^2))
   d <- round(d, round)
-  print(paste("Cohen's d = ",d, sep = ""))
-  outputlist <- list("d" = d)
+  return(d)
 }
 
