07224aba851aa9b6fbf149d7de9affde1808e79a
Samuel <kontakt@samueltobler.ch>
2023-01-05 10:30:58 +0100

    Function Update

--- R/p.signs.R
+++ R/p.signs.R
@@ -1,16 +1,33 @@
-p.signs <- function(data, margins = TRUE) {
+p.signs <- function(data, margins = TRUE, label.ns = TRUE) {
   vec <- vector()
+  if (label.ns == TRUE) {
   for(i in 1:length(data)) {
     if(margins == TRUE) {
       if(data[i] <= 0.1 & data[i] > 0.05){vec[i]= "†"} 
-      if(data[i] > 0.1 ){vec[i]= ""} 
+      if(data[i] > 0.1 ){vec[i]= "ns"} 
     } else {
-        if(data[i] > 0.05 ){vec[i]= ""}
+        if(data[i] > 0.05 ){vec[i]= "ns"}
     }
     if(data[i] <= 0.05 ){vec[i]= "*"} 
     if(data[i] <= 0.01 ){vec[i]= "**"} 
     if(data[i] <= 0.001 ){vec[i]= "***"} 
     if(data[i] <= 0.0001 ){vec[i]= "****"} 
+  }
+  }
+  
+  if (label.ns == FALSE) {
+    for(i in 1:length(data)) {
+      if(margins == TRUE) {
+        if(data[i] <= 0.1 & data[i] > 0.05){vec[i]= "†"} 
+        if(data[i] > 0.1 ){vec[i]= ""} 
+      } else {
+        if(data[i] > 0.05 ){vec[i]= ""}
+      }
+      if(data[i] <= 0.05 ){vec[i]= "*"} 
+      if(data[i] <= 0.01 ){vec[i]= "**"} 
+      if(data[i] <= 0.001 ){vec[i]= "***"} 
+      if(data[i] <= 0.0001 ){vec[i]= "****"} 
+    }
   }
   return(vec)
 }
